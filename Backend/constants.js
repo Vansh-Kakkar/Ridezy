7b23dab5d6066f1f15b946aafdbb8fd2cfc545bc
Vansh-Kakkar <v.kakkar1994@gmail.com>
2025-06-24 14:58:40 +0530

    user registration and login

---
+++ Backend/constants.js
@@ -0,0 +1,4 @@
+const DB_NAME="ridezy";
+
+
+module.exports=DB_NAME;
---
+++ Backend/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const DB_NAME= require('../constants.js')
+
+
+const  connectToDb = async()=> {
+       try {
+        const connectedInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`/n database connected !! DB Host: ${connectedInstance.connection.host}`)
+       } catch (error) {
+          console.error("error",error)
+          process.exit(1);
+       }
+}
+
+
+
+    module.exports = connectToDb;
